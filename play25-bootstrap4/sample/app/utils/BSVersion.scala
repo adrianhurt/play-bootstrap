81334582d3653292db2643fe0269a528386edd5e
Michael Stringer <stringbean@users.noreply.github.com>
2016-03-07 16:11:20 +0000

    Support Play 2.5.x

---
+++ play25-bootstrap4/sample/app/utils/BSVersion.scala
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2015 Adrian Hurtado (adrianhurt)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package utils
+
+object BSVersion {
+  final val code = "1.0-P24-B4"
+  final val library = "1.0"
+  final val play = "Play 2.4"
+  final val play_code = "2.4"
+  final val bootstrap = "Bootstrap 4"
+  final val bootstrap_code = "4"
+  final val gitBranch = code
+
+  final val msgsName = "messages"
+  final val msgsClass = "Messages"
+  final val msgsArg = s"$msgsName: $msgsClass"
+}
